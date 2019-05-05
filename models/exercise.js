44d39c54b03ef8fe445ff6737da58f117585dfc6
Glitch (skip-hendriks-exercise-tracker) <none>
2019-05-05 04:37:09 +0000

    exercise schema added

---
+++ models/exercise.js
@@ -0,0 +1,35 @@
+    
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Int32 = require('mongoose-int32');
+const shortid = require('shortid');
+const User = require('./user');
+
+var ExerciseSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    validate: {
+      validator: async (userId) => {
+        const user = await User.findOne({_id: userId});
+        return user !== null;
+      },
+      message: "userId doesn't exist"
+    }
+  },
+  description: String,
+  duration: {
+    type: Int32,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  _id: {
+    'type': String,
+    'default': shortid.generate
+  }
+});
+
+module.exports = mongoose.model('Exercise', ExerciseSchema);
