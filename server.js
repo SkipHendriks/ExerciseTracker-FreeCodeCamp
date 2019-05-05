52d2d3bbb6ff369168dbc1888f7e3a85a931ac5e
Glitch (skip-hendriks-exercise-tracker) <none>
2019-05-05 04:55:43 +0000

    moved synchronous verifications to seperate method

--- server.js
+++ server.js
@@ -41,33 +41,7 @@ app.post('/api/exercise/new-user', async (req, res, next) => {
 // [POST] endpoint for adding an exercise
 app.post('/api/exercise/add', async (req, res, next) => {
   try {
-    
-    // simple verifications
-    if (req.body.userId == '') {
-      const err = new Error("userId undefined");
-      err.status = 400;
-      throw err;
-    }
-    if (req.body.duration == '') {
-      const err = new Error("duration undefined");
-      err.status = 400;
-      throw err;
-    } else if (Number.isNaN(parseInt(req.body.duration))) {
-      const err = new Error("duration is NaN");
-      err.status = 400;
-      throw err;
-    }
-    if (req.body.date == '') {
-      const err = new Error("date undefined");
-      err.status = 400;
-      throw err;
-    } else if (!/^\d\d\d\d-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/.test(req.body.date)) {
-      const err = new Error("date format incorrect");
-      err.status = 400;
-      throw err;
-    }
-    
-    
+    inputFormatVerifications(req.body);
     const new_exercise = new Exercise ({userId: req.body.userId, description: req.body.description, duration: req.body.duration, date: new Date(req.body.date)});
     await new_exercise.save();
     res.json(new_exercise);
@@ -75,6 +49,34 @@ app.post('/api/exercise/add', async (req, res, next) => {
     next(err);
   }
 });
+
+const inputFormatVerifications = ({userId, duration, date}) => {
+  // simple synchronous verifications
+  if (userId == '') {
+    const err = new Error("userId undefined");
+    err.status = 400;
+    throw err;
+  }
+  if (duration == '') {
+    const err = new Error("duration undefined");
+    err.status = 400;
+    throw err;
+  } else if (Number.isNaN(parseInt(duration))) {
+    const err = new Error("duration is NaN");
+    err.status = 400;
+    throw err;
+  }
+  if (date == '') {
+    const err = new Error("date undefined");
+    err.status = 400;
+    throw err;
+  } else if (!/^\d\d\d\d-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/.test(date)) {
+    const err = new Error("date format incorrect");
+    err.status = 400;
+    throw err;
+  }
+};
+  
 
 // Not found middleware
 app.use((req, res, next) => {
