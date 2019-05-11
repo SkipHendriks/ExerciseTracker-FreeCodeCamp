231ee31e956b5e8bff4199636d0699ace34333d2
Glitch (skip-hendriks-exercise-tracker) <none>
2019-05-11 21:27:53 +0000

    updated packages

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// TODO: remove __v from responses using: https://www.npmjs.com/package/mongoose-hidden
 // TODO: https://medium.freecodecamp.org/how-to-write-a-production-ready-node-and-express-app-f214f0b17d8c
 // TODO: write tests
 // TODO: fix '../.../..' bullshit
@@ -6,6 +6,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import * as Promise from 'bluebird';
 
 import router from './routes/router.js';
 import errorHandler from'./middlewares/error-handler.js';
