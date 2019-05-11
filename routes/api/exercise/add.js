585f7e39065c57911adf5365091ddc5518b2a9d8
Glitch (skip-hendriks-exercise-tracker) <none>
2019-05-11 21:24:26 +0000

    adapted add endpoint to specs

--- routes/api/exercise/add.js
+++ routes/api/exercise/add.js
@@ -1,21 +1,51 @@
 import Exercise from '../../../models/exercise.js';
+import User from '../../../models/user.js';
 import {checkDateFormat} from '../../../utils/date.js';
 
 // [POST] endpoint for adding an exercise
 const addExercise = async (req, res, next) => {
   try {
     const {userId, description, duration, date:date_string} = req.body;
-    inputFormatVerifications(userId, duration, date);
-    const date = new Date(date_string);
-    const new_exercise = new Exercise ({userId, description, duration, date});
-    await new_exercise.save();
-    res.json(new_exercise);
+    inputVerifications(userId, duration, date_string);
+    const date = date_string ? new Date(date_string) : new Date();
+    // specifications ask for a username in reponse
+    // in my opinion it is better to return no other information than exercise schema
+    //   and providing a "/user" endpoint for finding username / _id matches
+    //   this saves a database call on all saves done here
+    let user = User.findOne({_id: userId });
+    let new_exercise = new Exercise ({userId, description, duration, date}).save();
+    // error handling could get wonky here since both user an new_exercise promises
+    //   will return an error on non-existant userId and either one could come up first
+    //   removing username from the result fixes this as well
+    [user, new_exercise] = await Promise.all([user, new_exercise]);
+    // specifications also require _id to be the userId, but I would rather use userId as
+    //   the name since it is a foreign id
+    //   since I wanted to keep my schema named right (using the name userId) I had to
+    //   do some swapping around here
+    const response = {...new_exercise.toObject(), _id: new_exercise.userId, username: user.username};
+    delete response.userId;
+    res.json(response);
   } catch (err) {
     next(err);
   }
 };
 
-const inputFormatVerifications = (userId, duration, date) => {
+/*
+// prefered version of function above
+const addExercise = async (req, res, next) => {
+  try {
+    const {userId, description, duration, date:date_string} = req.body;
+    inputVerifications(userId, duration, date_string);
+    const date = date_string ? new Date(date_string) : new Date();
+    let new_exercise = await new Exercise ({userId, description, duration, date}).save();
+    res.json(new_exercise);
+  } catch (err) {
+    next(err);
+  }
+};
+*/
+
+const inputVerifications = (userId, duration, date) => {
   // simple synchronous verifications
   if (userId == '') {
     const err = new Error("userId undefined");
@@ -31,11 +61,7 @@ const inputFormatVerifications = (userId, duration, date) => {
     err.status = 400;
     throw err;
   }
-  if (date == '') {
-    const err = new Error("date undefined");
-    err.status = 400;
-    throw err;
-  } else if (!checkDateFormat(date)) {
+  if (date && !checkDateFormat(date)) {
     const err = new Error("date format incorrect");
     err.status = 400;
     throw err;
