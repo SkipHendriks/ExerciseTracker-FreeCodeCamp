0772ea9a428e6ab2f6925df58a984328ef2ead52
Skip Hendriks <skappa@localhost.localdomain>
2019-07-18 17:05:35 +0930

    removed import.meta statement

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 import path from 'path';
-// ignore following Glitch error warning. works just fine
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 
 export default (req, res) => {
-  // Glitch doesn't like relative paths that go back (eg. '../') so path has to be resolved
-  res.sendFile(path.resolve(__dirname + '/../views/index.html')); 
+  const dir = path.resolve();
+  res.sendFile(path.resolve(`${dir}/views/index.html`));
 };
